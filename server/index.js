abdef2b426c389b450ef217824a66e153a9c91a9
John Galindez <jfrankgalindez@sipay.es>
2019-02-26 10:58:28 +0100

    adding socket.io

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const path = require('path')
 const express = require('express')
+const sockerIO = require('socket.io')
+const http = require('http')
 const app = express()
-const publicPath = path.join(__dirname + '../public/')
+const publicPath = path.join(__dirname, '../public')
+const server = http.createServer(app)
+const io = sockerIO(server)
+
+io.on('connection', socket => {
+  console.log('New user connected')
+  socket.on('disconnect', () => {
+    console.log('User has been disconnected')
+  })
+})
 
 app.use(express.static(publicPath))
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('------ Server is up on port 3000 ------')
 })
